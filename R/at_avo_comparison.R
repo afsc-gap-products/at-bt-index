a7d2879f41eb102f3119ccec453f5ec0d6249d3f
Sophia Wassermann <hi@sowasser.com>
2025-04-30 16:56:56 -0700

    New script for looking at AT/AVO data overlap

---
+++ R/at_avo_comparison.R
@@ -0,0 +1,61 @@
+# Exploration of AT & AVO data as included in the AT/BT overlap model.
+
+library(here)
+library(dplyr)
+library(ggplot2)
+library(sf)
+library(rnaturalearth)
+
+# Set ggplot theme
+if (!requireNamespace("ggsidekick", quietly = TRUE)) {
+  devtools::install_github("seananderson/ggsidekick")
+}
+library(ggsidekick)
+theme_set(theme_sleek())
+
+dat <- read.csv(here("data", "data_real.csv"))[, -9]
+dat_at <- dat %>% filter(Gear %in% c("AT2", "AT3"))
+dat_avo <- read.csv(here("data", "avo", "2018-AVO-1m-grid-cell.csv"))
+dat_avo <- cbind.data.frame(Lat = dat_avo$latitude,
+                            Lon = dat_avo$longitude,
+                            Year = 2018,
+                            sA = dat_avo$sA,
+                            Gear = "AVO",
+                            AreaSwept_km2 = 1,
+                            Vessel = "none",
+                            depth = dat_avo$height)
+
+# Annual plot of AT sampling location (as recorded here)
+lat_lon <- rbind.data.frame(cbind.data.frame())
+world <- ne_countries(scale = "medium", returnclass = "sf")
+sf_use_s2(FALSE)  # turn off spherical geometry
+map_at <- ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = dat_at, aes(x = Lon, y = Lat, color = Gear, shape = Gear), alpha = 0.3) +
+  coord_sf(xlim = c(-179, -157), ylim = c(54, 65), expand = FALSE) +
+  # scale_color_viridis(option = "plasma", discrete = FALSE, end = 0.9) +
+  scale_x_continuous(breaks = c(-178, -158)) +
+  scale_y_continuous(breaks = c(55, 64)) +
+  labs(x = NULL, y = NULL) +
+  theme(plot.background = element_rect(fill = "transparent")) +
+  facet_wrap(~ Year)
+map_at
+
+# Combined plot for 2018
+at_avo_loc <- rbind.data.frame(dat_at[, c(1:3, 5)] %>% filter(Gear == "AT2" & Year == 2018),
+                                    dat_avo[, c(1:3, 5)])
+at_avo_map <- ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = at_avo_loc, aes(x = Lon, y = Lat, color = Gear, shape = Gear), alpha = 0.7) +
+  coord_sf(xlim = c(-179, -157), ylim = c(54, 65), expand = FALSE) +
+  # scale_color_viridis(option = "plasma", discrete = FALSE, end = 0.9) +
+  scale_x_continuous(breaks = c(-178, -158)) +
+  scale_y_continuous(breaks = c(55, 64)) +
+  labs(x = NULL, y = NULL) +
+  theme(plot.background = element_rect(fill = "transparent")) 
+at_avo_map
+  
+
+# Depth-weighted abundance (biomass or backscatter) estimates for both surveys
+
+# Z-score datasets for basic comparison
