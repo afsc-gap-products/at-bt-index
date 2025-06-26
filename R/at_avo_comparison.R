0f7b2deba5a7bc687d488cfb968e3c9054efd754
Sophia Wassermann - NOAA <so@sowasser.com>
2025-06-25 17:20:04 -0700

    Added AT3 back into some comparisons & disaggregated AVO by depth bins

--- R/at_avo_comparison.R
+++ R/at_avo_comparison.R
@@ -17,15 +17,15 @@ theme_set(theme_sleek())
 
 # Combine data ----------------------------------------------------------------
 dat <- read.csv(here("data", "data_real.csv"))[, -9]
-dat_avo <- read.csv(here("data", "avo", "2018-AVO-1m-grid-cell.csv"))
-dat_avo <- cbind.data.frame(Lat = dat_avo$latitude,
-                            Lon = dat_avo$longitude,
+avo_original <- read.csv(here("data", "avo", "2018-AVO-1m-grid-cell.csv"))
+dat_avo <- cbind.data.frame(Lat = avo_original$latitude,
+                            Lon = avo_original$longitude,
                             Year = 2018,
-                            sA = dat_avo$sA,
+                            sA = avo_original$sA,
                             Gear = "AVO",
                             AreaSwept_km2 = 1,
                             Vessel = "none",
-                            depth = dat_avo$height)
+                            depth = avo_original$height)
   
 # Annual plot of AT sampling location (as recorded here) ----------------------
 lat_lon <- rbind.data.frame(cbind.data.frame())
@@ -43,40 +43,18 @@ map_at <- ggplot(data = world) +
 map_at
 
 # Combined plot of locations for 2018 -----------------------------------------
-all_dat <- rbind.data.frame(dat[, c(1:3, 5, 8)] %>% filter(Gear != "AT3" & Year == 2018),
+all_dat <- rbind.data.frame(dat[, c(1:3, 5, 8)] %>% filter(Year == 2018),
                             dat_avo[, c(1:3, 5, 8)])
 at_avo_map <- ggplot(data = world) +
   geom_sf() +
-  geom_point(data = all_dat, aes(x = Lon, y = Lat, color = Gear, shape = Gear), alpha = 0.7) +
+  geom_point(data = all_dat %>% filter(Gear != "AT3"), 
+             aes(x = Lon, y = Lat, color = Gear, shape = Gear), alpha = 0.7) +
   coord_sf(xlim = c(-179, -157), ylim = c(54, 65), expand = FALSE) +
   scale_x_continuous(breaks = c(-178, -158)) +
   scale_y_continuous(breaks = c(55, 64)) +
   labs(x = NULL, y = NULL) +
   theme(plot.background = element_rect(fill = "transparent")) 
 at_avo_map
-
-# Mean depth (comparing AT2 & AT3 gear labels) - they're the same?
-at_depths <- dat %>% 
-  filter(Gear %in% c("AT2", "AT3")) %>%
-  group_by(Gear, Year) %>%
-  summarize(mean_depth = mean(depth))
-
-# Plot of mean depth by survey ------------------------------------------------
-mean_depth_loc <- all_dat %>%
-  filter(Gear != "AT3") %>%
-  group_by(Gear, Lat, Lon) %>%
-  summarize(mean_depth = mean(depth))
-
-mean_depth <- ggplot(data = world) +
-  geom_sf() +
-  geom_point(data = mean_depth_loc, aes(x = Lon, y = Lat, color = mean_depth), shape = "square", size = 2) +
-  coord_sf(xlim = c(-179, -157), ylim = c(54, 65), expand = FALSE) +
-  scale_x_continuous(breaks = c(-178, -158)) +
-  scale_y_continuous(breaks = c(55, 64)) +
-  scale_color_viridis(option = "mako", direction = -1, guide = guide_colorbar(reverse = TRUE)) +
-  labs(x = NULL, y = NULL) +
-  facet_wrap(~ Gear)
-mean_depth
 
 # Plot of mean abundance by survey --------------------------------------------
 abundance <- function(df, gear) {
@@ -103,9 +81,10 @@ abundance <- function(df, gear) {
 
 mean_abundance <- plot_grid(
   abundance(dat, gear = "AT2"), 
+  abundance(dat, gear = "AT3"), 
   abundance(dat_avo, gear = "AVO"), 
   abundance(dat, gear = "BT"), 
-  ncol = 3)
+  ncol = 2)
 mean_abundance
 
 # Abundance-at-depth by survey ------------------------------------------------
@@ -127,7 +106,43 @@ abundance_depth <- function(df, gear, label) {
 
 abund_depth_plot <- plot_grid(
   abundance_depth(dat, gear = "AT2", label = "Abundance (kg)"), 
+  abundance_depth(dat, gear = "AT3", label = "Abundance (kg)"), 
   abundance_depth(dat_avo, gear = "AVO", label = "Abundance (sA)"), 
   abundance_depth(dat, gear = "BT", label = "Abundance (kg)"), 
-  ncol = 3)
+  ncol = 2)
 abund_depth_plot
+
+# Partition AVO into depth layers & plot proportion of backscatter ------------
+# Calculate total backscatter for proportions
+total_sA <- avo_original %>%
+  group_by(station, latitude, longitude) %>%
+  summarize(sA = sum(sA))
+
+# Disaggregate by depth strata & calculate proportions
+# AVO1 <- dat_avo %>% filter(depth <= 0.5)  # no fish at less that 0.5m, as expected!
+AVO2 <- avo_original %>% 
+  filter(between(height, 0.5, 16)) %>%
+  group_by(station, latitude, longitude)  %>%
+  summarise(sA = sum(sA)) 
+AVO2$proportion <- AVO2$sA / total_sA$sA
+AVO2$gear <- "AVO2"
+
+AVO3 <- avo_original %>% 
+  filter(height >= 16) %>%  # not sure what was inclusive of 16 before & there are no observations at 16m
+  group_by(station, latitude, longitude)  %>%
+  summarise(sA = sum(sA)) 
+AVO3$proportion <- AVO3$sA / total_sA$sA
+AVO3$gear <- "AVO3"
+
+avo_prop <- ggplot(data = world) +
+  geom_sf() +
+  geom_point(data = rbind.data.frame(AVO2, AVO3), 
+             aes(x = longitude, y = latitude, color = proportion), 
+             shape = "square", size = 2) +
+  coord_sf(xlim = c(-179, -157), ylim = c(54, 65), expand = FALSE) +
+  scale_x_continuous(breaks = c(-178, -158)) +
+  scale_y_continuous(breaks = c(55, 64)) +
+  scale_color_viridis(option = "mako", direction = -1) +
+  labs(x = NULL, y = NULL) +
+  facet_wrap(~gear)
+avo_prop
