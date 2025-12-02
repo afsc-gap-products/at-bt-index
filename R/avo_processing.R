ae831c8be63ee9c0957fc25f9de04574e6552c5f
Sophia Wassermann - NOAA <so@sowasser.com>
2025-12-02 15:54:33 -0800

    Updated AVO data processing (new folder structure, better plot, etc.)

--- R/avo_processing.R
+++ R/avo_processing.R
@@ -1,6 +1,6 @@
-#' Script for disaggregating the acoustic vessel of opportunity (AVO) index by
-#' depth layer: 0.5m above the bottom, 0.5m above the bottom to 16m from the
-#' bottom, and 16m from the surface and above
+#' STEP 2: Script for disaggregating the acoustic vessel of opportunity (AVO) 
+#' index by depth layer: 0.5m above the bottom, 0.5m above the bottom to 16m 
+#' from the bottom, and 16m from the surface and above
 
 library(here)
 library(dplyr)
@@ -17,21 +17,25 @@ if (!requireNamespace("ggsidekick", quietly = TRUE)) {
 library(ggsidekick)
 theme_set(theme_sleek())
 
+# Set up output folder and processing inputs ----------------------------------
+year <- format(Sys.Date(), "%Y")
+wd <- here("data", year)
+
 # Whether to make the plots when running the code (set to FALSE to save time)
-plotting <- FALSE
+plotting <- TRUE
 
 # Year set for AVO
-avo_years <- c(2009, 2010, 2012, 2014:2018)
+avo_years <- c(2009, 2010, 2012, 2014:2019, 2021:2025)
 
 # Read in & update haul information
-hauls <- read.csv(here("data", "hauls.csv")) %>%
+hauls <- read.csv(here(wd, "hauls.csv")) %>%
   filter(year %in% avo_years) %>%
   rename(station = stationid)
 
 # Read in AVO data & disaggregate by depth/height -----------------------------
 avo_original <- data.frame()
 for(i in avo_years) {
-  df <- read.csv(here("data", "avo", paste0(i, "-AVO-1m-grid-cell-bottom-ref.csv")))
+  df <- read.csv(here("data", "avo", paste0(i, "-AVO-1m-grid-cell.csv")))
   df$year <- i
   avo_original <- rbind.data.frame(avo_original, df)
 }
@@ -53,7 +57,7 @@ AVO3 <- avo_joined %>%
 avo_processed <- rbind.data.frame(AVO2, AVO3) %>%
   select(year, latitude, longitude, station, sA, height, from_surface, gear)
 
-write.csv(avo_processed, file = here("data", "avo", "avo_processed.csv"))
+write.csv(avo_processed, file = here(wd, "avo_processed.csv"))
   
 # Calculate total backscatter for proportions
 total_sA <- avo_original %>%
@@ -82,23 +86,6 @@ AVO3_prop <- AVO3 %>%
 if(plotting == TRUE) {
   world <- ne_countries(scale = "medium", returnclass = "sf")
   sf_use_s2(FALSE)  # turn off spherical geometry
-  avo_map <- ggplot(data = world) +
-    geom_sf() +
-    geom_tile(data = avo_processed,
-              aes(x = longitude, y = latitude, fill = sA),
-              width = 0.55, height = 0.3) +
-    coord_sf(xlim = c(-179, -157), ylim = c(53.8, 63.5), expand = FALSE) +
-    scale_fill_viridis(option = "mako", direction = -1) +
-    theme(axis.title.x=element_blank(),
-          axis.text.x=element_blank(),
-          axis.ticks.x=element_blank()) +
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x = NULL, y = NULL) +
-    facet_grid(gear ~ year) +
-    theme(legend.position = "bottom") 
-  avo_map
   
   avo_prop <- ggplot(data = world) +
     geom_sf() +
@@ -114,12 +101,16 @@ if(plotting == TRUE) {
           axis.text.y=element_blank(),
           axis.ticks.y=element_blank()) +
     labs(x = NULL, y = NULL) +
-    facet_grid(gear ~ year) +
-    theme(legend.position = "bottom") 
-  avo_prop
+    theme(legend.position = "bottom") +
+    ggforce::facet_grid_paginate(gear ~ year, ncol = 7, nrow = 2, page = 1) 
   
-  ggsave(avo_prop, filename = here("Results", "avo exploration", "avo_proportion.png"),
-         width = 250, height = 80, units = "mm", dpi = 300)
+  avo_prop2 <- avo_prop +
+    ggforce::facet_grid_paginate(gear ~ year, ncol = 7, nrow = 2, page = 2)
+
+  ggsave(avo_prop, filename = here("Results", "avo exploration", "avo_prop1.png"),
+         width = 250, height = 100, units = "mm", dpi = 300)
+  ggsave(avo_prop2, filename = here("Results", "avo exploration", "avo_prop2.png"),
+         width = 250, height = 100, units = "mm", dpi = 300)
 }
 
 # Combine with original dataset & create a new dataframe ----------------------
@@ -138,5 +129,5 @@ avo_out <- avo_processed %>%
          Gear = gear)
 
 write.csv(rbind.data.frame(dat_new, avo_out), 
-          file = here("data", "at_bt_avo_binned_all.csv"),
+          file = here(wd, "at_bt_avo_binned_all.csv"),
           row.names = FALSE)
