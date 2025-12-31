dade100672e99d045bbe503875fcb4bca98ecfbd
Sophia Wassermann <hi@sowasser.com>
2025-12-31 12:13:12 -0800

    Updated model comparison plots to include 4-layer model w/o AVO

--- R/model_comparison.R
+++ R/model_comparison.R
@@ -22,42 +22,42 @@ read_model <- function(wd, filetype) {
 # Availability to gear across models ------------------------------------------
 gear_results <- bind_rows(read_model("4 layers", "availability_gear.csv"),
                           read_model("no AVO 3-16", "availability_gear.csv"),
-                          read_model("no AVO 16", "availability_gear.csv"))
+                          read_model("no AVO 16", "availability_gear.csv"),
+                          read_model("no AVO 4-layer", "availability_gear.csv"))
+gear_results$model <- factor(gear_results$model, 
+                             levels = c("4 layers", "no AVO 4-layer", "no AVO 3-16", "no AVO 16"))
 
 ggplot() +
   geom_line(data = gear_results, 
-            aes(x = Year, y = Proportion, color = model)) +
+            aes(x = Year, y = Proportion)) +
   geom_ribbon(data = gear_results, 
-              aes(x = Year, ymin = (Proportion - 2 * SD), ymax = (Proportion + 2 * SD), fill = model), alpha = 0.4) +
-  scale_color_viridis(discrete = TRUE, direction = -1, end = 0.9) +
-  scale_fill_viridis(discrete = TRUE, direction = -1, end = 0.9) +
+              aes(x = Year, ymin = (Proportion - 2 * SD), ymax = (Proportion + 2 * SD)), alpha = 0.4) +
   ylab("Proportion available") + xlab("") +
-  facet_wrap(~ Gear)
+  facet_grid(model ~ Gear)
 
 ggsave(filename = here("Results", "model_compare_gear.png"), 
-       width = 240, height = 90, units = "mm", dpi = 300)
+       width = 200, height = 200, units = "mm", dpi = 300)
 
 # Index by depth across models ------------------------------------------------
 index_results <- bind_rows(read_model("4 layers", "index_depth.csv"),
                            read_model("no AVO 3-16", "index_depth.csv"),
-                           read_model("no AVO 16", "index_depth.csv"))
+                           read_model("no AVO 16", "index_depth.csv"),
+                           read_model("no AVO 4-layer", "index_depth.csv"))
+index_results$model <- factor(index_results$model, 
+                             levels = c("4 layers", "no AVO 4-layer", "no AVO 3-16", "no AVO 16"))
 index_results$Height <- factor(index_results$Height, 
-                               levels = c(">16m", "3-16m", "0.5-3m", "<0.5m"))
+                               levels = c("<0.5m", "0.5-3m", "3-16m", ">16m"))
 
 ggplot() +
   geom_line(data = index_results, 
-            aes(x = Year, y = Estimate, color = model)) +
-  # geom_point(data = survey_yr_points,
-  #            aes(x = Year, y = Proportion, color = Gear, shape = Gear)) +
+            aes(x = Year, y = Estimate)) +
   geom_ribbon(data = index_results, 
-              aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD), fill = model), alpha = 0.4) +
-  scale_color_viridis(discrete = TRUE, direction = -1, end = 0.9) +
-  scale_fill_viridis(discrete = TRUE, direction = -1, end = 0.9) +
+              aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD)), alpha = 0.4) +
   ylab("Index of Abundance (Mt)") + xlab("") +
-  facet_wrap(~ Height)
+  facet_grid(model ~ Height)
 
 ggsave(filename = here("Results", "model_compare_depth.png"), 
-       width = 240, height = 150, units = "mm", dpi = 300)
+       width = 240, height = 180, units = "mm", dpi = 300)
 
 # Total index of abundance across models --------------------------------------
 total_index <- index_results %>% 
@@ -66,11 +66,11 @@ total_index <- index_results %>%
             SD = sum(SD)) 
 
 ggplot(total_index) +
+  geom_ribbon(aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD), fill = model), alpha = 0.2) +
   geom_line(aes(x = Year, y = Estimate, color = model)) +
-  geom_ribbon(aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD), fill = model), alpha = 0.4) +
-  scale_color_viridis(discrete = TRUE, direction = -1, end = 0.9) +
-  scale_fill_viridis(discrete = TRUE, direction = -1, end = 0.9) +
-  ylab("Index of Abundance (Mt)") + xlab("")
+  scale_color_viridis(discrete = TRUE, end = 0.9) +
+  scale_fill_viridis(discrete = TRUE, end = 0.9) +
+  ylab("Index of Abundance (Mt)") + xlab("") 
 
 ggsave(filename = here("Results", "total_index_compare.png"),
        width = 150, height = 90, units = "mm", dpi = 300)
