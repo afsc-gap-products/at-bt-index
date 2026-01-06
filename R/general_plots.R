c99d06e7f4751265e49eb92ff1d6e023f8050c93
Sophia Wassermann - NOAA <so@sowasser.com>
2026-01-05 17:13:23 -0800

    More WKUSER versions of plots (index by depth & comparison plots)

--- R/general_plots.R
+++ R/general_plots.R
@@ -218,3 +218,102 @@ avail_both
 
 ggsave(avail_both, filename = here(dir, "avail_both_avo.png"),
        width = 150, height = 150, units = "mm", dpi = 300)
+
+# Index by depth
+ind_depth <- read.csv(here("Results", "no AVO 4-layer", "index_depth.csv")) %>%
+  mutate(Height = factor(Height, levels = c(">16m", "3-16m", "0.5-3m", "<0.5m")))
+
+ind_depth_plot <- ggplot() +
+  geom_line(data = ind_depth, 
+            aes(x = Year, y = Estimate, color = Height)) +
+  # geom_point(data = survey_yr_points,
+  #            aes(x = Year, y = Proportion, color = Gear, shape = Gear)) +
+  geom_ribbon(data = ind_depth, 
+              aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD), fill = Height), alpha = 0.4) +
+  scale_color_viridis(option = "mako", discrete = TRUE, direction = -1, begin = 0.3) +
+  scale_fill_viridis(option = "mako", discrete = TRUE, direction = -1, begin = 0.3) +
+  ylab("Index of Abundance (Mt)") + xlab("")
+ind_depth_plot
+
+ggsave(ind_depth_plot, filename = here(dir, "index_depth_plot.png"),
+       width = 120, height = 70, units = "mm", dpi = 300)
+
+# Comparison plots ------------------------------------------------------------
+# Function for reading in model results 
+read_model <- function(wd, filetype) {
+  df <- read.csv(here("results", wd, filetype))
+  df$model <- wd
+  return(df)
+}
+
+# Availability to gear across models 
+gear_results <- bind_rows(read_model("4 layers", "availability_gear.csv"),
+                          read_model("no AVO 3-16", "availability_gear.csv"),
+                          read_model("no AVO 16", "availability_gear.csv"),
+                          read_model("no AVO 4-layer", "availability_gear.csv"))
+gear_results$model <- factor(gear_results$model, 
+                             levels = c("4 layers", "no AVO 4-layer", "no AVO 3-16", "no AVO 16"))
+
+ggplot() +
+  geom_line(data = gear_results, 
+            aes(x = Year, y = Proportion), color = "#2FB47C") +
+  geom_ribbon(data = gear_results, 
+              aes(x = Year, ymin = (Proportion - 2 * SD), ymax = (Proportion + 2 * SD)), fill = "#2FB47C", alpha = 0.4) +
+  ylab("Proportion available") + xlab("") +
+  facet_grid(Gear ~ model)
+
+ggsave(filename = here("Results", "wkuser", "model_compare_gear.png"), 
+       width = 200, height = 90, units = "mm", dpi = 300)
+
+# Index by depth across models 
+index_results <- bind_rows(read_model("4 layers", "index_depth.csv"),
+                           read_model("no AVO 3-16", "index_depth.csv"),
+                           read_model("no AVO 16", "index_depth.csv"),
+                           read_model("no AVO 4-layer", "index_depth.csv"))
+index_results$model <- factor(index_results$model, 
+                              levels = c("4 layers", "no AVO 4-layer", "no AVO 3-16", "no AVO 16"))
+index_results$Height <- factor(index_results$Height, 
+                               levels = c("<0.5m", "0.5-3m", "3-16m", ">16m"))
+
+ggplot() +
+  geom_line(data = index_results, 
+            aes(x = Year, y = Estimate), color = "#2FB47C") +
+  geom_ribbon(data = index_results, 
+              aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD)), 
+              fill = "#2FB47C", alpha = 0.4) +
+  ylab("Index of Abundance (Mt)") + xlab("") +
+  facet_grid(model ~ Height)
+
+ggsave(filename = here("Results", "wkuser", "model_compare_depth.png"), 
+       width = 240, height = 180, units = "mm", dpi = 300)
+
+# Total index of abundance across models 
+total_index <- index_results %>% 
+  group_by(model, Year) %>%
+  summarize(Estimate = sum(Estimate),
+            SD = sum(SD)) 
+
+# Read in sdmTMB index, select EBS only, at-bt model date range, convert to Mt
+index <- readRDS(here("data", "indices.RDS")) %>% 
+  filter(stratum == "EBS") %>%
+  filter(year %in% min(index_results$Year):max(index_results$Year)) %>%
+  mutate(est = est / 1e9, 
+         lwr = lwr / 1e9,
+         upr = upr / 1e9)
+
+ggplot() +
+  geom_ribbon(data = total_index, 
+              aes(x = Year, ymin = (Estimate - 2 * SD), ymax = (Estimate + 2 * SD), fill = model), 
+              alpha = 0.2) +
+  geom_line(data = total_index, 
+            aes(x = Year, y = Estimate, color = model)) +
+  geom_ribbon(data = index,
+              aes(x = year, ymin = lwr, ymax = upr), fill = "red", alpha = 0.2) +
+  geom_line(data = index,
+            aes(x = year, y = est), color = "red") +
+  scale_color_viridis(discrete = TRUE, end = 0.9) +
+  scale_fill_viridis(discrete = TRUE, end = 0.9) +
+  ylab("Index of Abundance (Mt)") + xlab("") 
+
+ggsave(filename = here("Results", "wkuser", "total_index_compare.png"),
+       width = 150, height = 90, units = "mm", dpi = 300)
